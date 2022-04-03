a9b1a4d86bdb2403bd3d40ed3a295433b6e7cbee
viktor <viktor.nikolaenko.2005@gmail.com>
2022-04-03 14:08:19 +0300

    cc

--- app/main.py
+++ app/main.py
@@ -10,7 +10,10 @@ from data import db_session
 import random
 import os
 
+
 app = Flask(__name__, template_folder='templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/blogs.db'
+
 app.config['SECRET_KEY'] = 'b0-sdb-0sbdfb0-bgf0sb-db0vf'
 db_session.global_init("db/blogs.db")
 login_manager = LoginManager()
