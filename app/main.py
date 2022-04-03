d5bf68c7f1b120cba70a0349318d880618465bc1
viktor <viktor.nikolaenko.2005@gmail.com>
2022-04-03 13:58:59 +0300

    cc

--- app/main.py
+++ app/main.py
@@ -286,4 +286,5 @@ def create(id_user):
 
 
 if __name__ == '__main__':
-    app.run(port=5000, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
